bf70fbacb4a21c80536a7933cc316572f412dd1c
bryan lin <920616bryanlin@gmail.com>
2025-02-27 16:27:13 +0800

    feat: search table from go client

--- main.go
+++ main.go
@@ -2,34 +2,56 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	Manticoresearch "github.com/manticoresoftware/manticoresearch-go"
 )
 
 func main() {
+	searchWenyan()
+	println("searchWenyan called")
+}
 
+func searchWenyan() {
 	// Create an instance of API client
 	configuration := Manticoresearch.NewConfiguration()
 	configuration.Servers[0].URL = "http://localhost:9308"
 	apiClient := Manticoresearch.NewAPIClient(configuration)
 
-	// Perform insert and search operations
-	tableName := "products"
-	indexDoc := map[string]interface{}{"title": "Crossbody Bag with Tassel"}
-	indexReq := Manticoresearch.NewInsertDocumentRequest(tableName, indexDoc)
-	indexReq.SetId(1)
-
-	apiClient.IndexAPI.Insert(context.Background()).InsertDocumentRequest(*indexReq).Execute()
-
+	tableName := "wenyan"
 	searchRequest := Manticoresearch.NewSearchRequest(tableName)
 	searchQuery := Manticoresearch.NewSearchQuery()
-	searchQuery.QueryString = "@title Bag"
-	searchRequest.Query = searchQuery
-	queryHighlight := Manticoresearch.NewHighlight()
-	queryHighlight.Fields = map[string]interface{}{"title": map[string]interface{}{}}
-	searchRequest.Highlight = queryHighlight
 
-	_, httpRes, _ := apiClient.SearchAPI.Search(context.Background()).SearchRequest(*searchRequest).Execute()
-	fmt.Printf("%+v\n\n", httpRes)
+	// encodedURL := url.QueryEscape("https://zh.wikisource.org/wiki?")
+	// searchQuery.QueryString = "@url " + encodedURL
+	searchQuery.QueryString = "@text 屈原"
+	searchRequest.Query = searchQuery
+
+	// Execute search
+	resp, _, err := apiClient.SearchAPI.Search(context.Background()).SearchRequest(*searchRequest).Execute()
+	if err != nil {
+		fmt.Printf("Error executing search: %v\n", err)
+		return
+	}
+
+	/* Save result to a json file*/
+	// Create or open the output JSON file
+	file, err := os.Create("search_result.json")
+	if err != nil {
+		fmt.Printf("Error creating file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	// Encode the response to the JSON file
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ") // Pretty-print the JSON with indentation
+	if err := encoder.Encode(resp); err != nil {
+		fmt.Printf("Error encoding to JSON: %v\n", err)
+		return
+	}
+
+	fmt.Println("Search results have been saved to search_result.json")
 }
